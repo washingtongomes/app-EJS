b4fa4e5fc3e33c3288ce5e4ed1e859f5757447d7
Washington Gomes <wsgomes600@gmail.com>
2022-12-06 21:25:37 -0300

    atualizando api-rest v3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import * as ArticleService from './ArticleService.js';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT||3000;
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
